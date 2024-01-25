62d867e2cbed86ff1f49d8002f8c84cc7f8e387d
SoftdevAshish <softdev.ashish@gmail.com>
2024-01-25 23:30:08 +0545

    feature: manage file and folder

--- config/config.ts
+++ config/config.ts
@@ -4,3 +4,9 @@ import * as process from 'process';
 config();
 
 export const appPort = process.env.APP_PORT;
+
+//Swagger Documentation
+export const swaggerTitle = process.env.API_TITLE;
+export const swaggerDescription = process.env.API_DESCRIPTION;
+export const swaggerVersion = process.env.API_VERSION;
+export const swaggerDocs = process.env.API_DOCS;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { appPort } from '../config/config';
+import {
+  appPort,
+  swaggerDescription,
+  swaggerDocs,
+  swaggerTitle,
+  swaggerVersion,
+} from '../config/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new ResponseTransformInterceptor());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'token',
+    )
+    .setTitle(swaggerTitle)
+    .setDescription(swaggerDescription)
+    .setVersion(swaggerVersion)
+    .addTag(swaggerDocs)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document, {
+    swaggerOptions: { docExpansion: 'none', persistAuthorization: true },
+  });
   await app.listen(appPort);
 }
+
 bootstrap();
